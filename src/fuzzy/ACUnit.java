8b6851ad491ce64dc0e6f60edea973a1ad43a2ba
Ethan Pollitt <ethanpollitt@gmail.com>
2014-05-06 16:06:59 -0400

    Update and rename TempController.java to ACUnit.java

---
+++ src/fuzzy/ACUnit.java
@@ -0,0 +1,53 @@
+package fuzzy;
+
+public class ACUnit extends Controller  {
+ 
+	private final double coolingFactor = .025;		// Absolute temperature change (in F) at 100% usage
+	private double currentLoad = 1;					// Load of the system (determined by inputSignal) 0 <= l <= 1
+	private SystemMode systemMode = SystemMode.OFF;	// -1 for cool, 0 for off, 1 for heat
+	
+	private Room room;
+	
+	private enum SystemMode {
+		COOL, HEAT, OFF
+	}
+	
+	public ACUnit(String controllerName, Room room) {
+		this.room = room;
+		this.pollingRate = 1;
+		this.controllerName = controllerName;
+	}
+	
+	public void process() throws InterruptedException {
+		int pollingTime = 0;				
+		double newTemp = this.room.temperature;
+		if(this.systemMode == SystemMode.COOL) {
+			newTemp = this.room.temperature - (this.coolingFactor * this.currentLoad);
+		} else if(this.systemMode == SystemMode.HEAT) {
+			newTemp = this.room.temperature + (this.coolingFactor * this.currentLoad);			
+		}
+		this.room.temperature = newTemp;
+			
+		Thread.sleep(this.pollingRate * 1000);
+	}
+	
+	public void setSignal(int signal) {
+		this.currentSignal = signal;
+		if(this.currentSignal == 0) {
+			this.systemMode = SystemMode.OFF;
+		} else if(this.currentSignal < 0) { 
+			this.systemMode = SystemMode.COOL;
+		} else if(this.currentSignal > 0) {
+			this.systemMode = SystemMode.HEAT;
+		}
+		
+		if(Math.abs(this.currentSignal) == 5) {
+			this.currentLoad = 1;
+		} else if(Math.abs(this.currentSignal) == 3) {
+			this.currentLoad = .5;
+		}
+		// TODO: Add more input levels here
+		
+		this.log("recieved new signal!", NotificationType.NOTIFY);
+	}
+}
