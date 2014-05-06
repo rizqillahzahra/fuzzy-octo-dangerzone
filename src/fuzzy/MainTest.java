5be245a83fb15551545418aacc91813e443c6780
Ethan Pollitt <ethanpollitt@gmail.com>
2014-05-06 16:07:11 -0400

    Update MainTest.java

--- src/fuzzy/MainTest.java
+++ src/fuzzy/MainTest.java
@@ -3,8 +3,10 @@ package fuzzy;
 public class MainTest {
 	public static void main(String[] args) {
 		Room room = new Room(81);
-		TempController tc = new TempController(room);
-		FuzzyController fc = new FuzzyController(tc, 72);
+		ACUnit tc = new ACUnit("AC_1", room);
+		
+		Sensor s = new Sensor(room);
+		FuzzyThermostat fc = new FuzzyThermostat("FUZZY_THERM_1", tc, s, 72);
 		
 		Thread fct = new Thread(fc);
 		Thread tct = new Thread(tc);
