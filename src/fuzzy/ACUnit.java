0876dc8165d52bc19b9500f2489983bd1a002c35
Ethan Pollitt <ethanpollitt@gmail.com>
2014-05-07 10:53:42 -0400

    Update ACUnit.java

--- src/fuzzy/ACUnit.java
+++ src/fuzzy/ACUnit.java
@@ -2,7 +2,7 @@ package fuzzy;
 
 public class ACUnit extends Controller  {
  
-	private final double coolingFactor = .025;		// Absolute temperature change (in F) at 100% usage
+	private final double coolingFactor = .02;		// Absolute temperature change (in F) at 100% usage
 	private double currentLoad = 1;					// Load of the system (determined by inputSignal) 0 <= l <= 1
 	private SystemMode systemMode = SystemMode.OFF;	// -1 for cool, 0 for off, 1 for heat
 	
@@ -18,8 +18,7 @@ public class ACUnit extends Controller  {
 		this.controllerName = controllerName;
 	}
 	
-	public void process() throws InterruptedException {
-		int pollingTime = 0;				
+	public void process() throws InterruptedException {			
 		double newTemp = this.room.temperature;
 		if(this.systemMode == SystemMode.COOL) {
 			newTemp = this.room.temperature - (this.coolingFactor * this.currentLoad);
@@ -31,7 +30,7 @@ public class ACUnit extends Controller  {
 		Thread.sleep(this.pollingRate * 1000);
 	}
 	
-	public void setSignal(int signal) {
+	public void setSignal(double signal) {
 		this.currentSignal = signal;
 		if(this.currentSignal == 0) {
 			this.systemMode = SystemMode.OFF;
@@ -44,7 +43,9 @@ public class ACUnit extends Controller  {
 		if(Math.abs(this.currentSignal) == 5) {
 			this.currentLoad = 1;
 		} else if(Math.abs(this.currentSignal) == 3) {
-			this.currentLoad = .5;
+			this.currentLoad = .66;
+		} else if(Math.abs(this.currentSignal) == 1.5) {
+			this.currentLoad = .33;
 		}
 		// TODO: Add more input levels here
 		
